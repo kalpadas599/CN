3ff9876bf1fc5c6fcadf8b1e3af1be8fbab595cd
Souradipta Saha <137396269+Souradipta3@users.noreply.github.com>
2025-06-06 21:43:54 +0530

    Create UDP_client_thread.java

---
+++ UDP_client_thread.java
@@ -0,0 +1,43 @@
+import java.net.*;
+import java.io.*;
+
+public class UDPThreadClient {
+    public static void main(String[] args) throws IOException {
+        DatagramSocket clientSocket = new DatagramSocket();
+        InetAddress serverIP = InetAddress.getByName("127.0.0.1");
+
+        new Thread(() -> {
+            try {
+                while (true) {
+                    byte[] receiveBuffer = new byte[1024];
+                    DatagramPacket receivePacket = new DatagramPacket(receiveBuffer, receiveBuffer.length);
+                    clientSocket.receive(receivePacket);
+
+                    String serverMessage = new String(receivePacket.getData(), 0, receivePacket.getLength());
+                    System.out.println("\nServer: " + serverMessage);
+                }
+            } catch (SocketException e) {
+                
+                System.out.println("Connection closed. Exiting receiver thread...");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+
+        BufferedReader clientInput = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            String clientMessage = clientInput.readLine();
+            byte[] sendBuffer = clientMessage.getBytes();
+
+            DatagramPacket sendPacket = new DatagramPacket(sendBuffer, sendBuffer.length, serverIP, 9876);
+            clientSocket.send(sendPacket);
+
+            if (clientMessage.equalsIgnoreCase("bye")) {
+                System.out.println("Disconnected from server.");
+                clientSocket.close();
+                System.exit(0);
+            }
+        }
+    }
+}
