dd1648dc12bec5d866241c9a4b0aa95a2b64b26f
Souradipta Saha <137396269+Souradipta3@users.noreply.github.com>
2025-06-06 21:05:02 +0530

    Create BasicClient.java

---
+++ BasicClient.java
@@ -0,0 +1,16 @@
+import java.net.*;
+import java.io.*;
+
+class BasicClient
+{
+public static void main(String arg[])
+{
+try
+{
+Socket clisc=new Socket("localhost",3000);
+System.out.println("*****Find Server******");
+}
+catch(Exception e){
+}
+}
+}
