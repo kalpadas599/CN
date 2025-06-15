ffead476dd65307ab9dec56b0aeb8de259d5a6f5
Kalpa Das <kalpadas599@gmail.com>
2025-06-15 17:38:33 +0530

    Update UDP_multi_server.java

--- UDP_multi_server.java
+++ UDP_multi_server.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.net.*;
 
 
-public class Server {
+public class UDP_multi_server {
 
    
     public static void main(String[] args) throws SocketException, IOException {
@@ -18,27 +18,20 @@ public class Server {
           DatagramPacket recvdpkt = new DatagramPacket(receivebuffer, receivebuffer.length);
           serverSocket.receive(recvdpkt);
           
-          //get the ip address and port no for client
           InetAddress IP = recvdpkt.getAddress();
           int portno = recvdpkt.getPort();
 
-          //receive client message converted into string
           String clientdata = new String(recvdpkt.getData());
 
-          //print the client message
           System.out.println("\nClient : "+ clientdata);
           
-          //printing the caption of server for type the message
           System.out.print("\nServer : ");
           
-          //create a buffer for keyboard input from user
           BufferedReader serverRead = new BufferedReader(new InputStreamReader (System.in) );
           String serverdata = serverRead.readLine();
           
-          //typecasting the user message into bytes
           sendbuffer = serverdata.getBytes();
           
-          //create a datagram and send the message to client 
           DatagramPacket sendPacket = new DatagramPacket(sendbuffer, sendbuffer.length, IP,portno);
           serverSocket.send(sendPacket); 
          
@@ -49,7 +42,6 @@ public class Server {
           }
                   
       }
-        //close the connection
         serverSocket.close();
     }
             
