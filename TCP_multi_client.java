5eb5776713be571a2c71ca575b4ac8572cb5cd29
Kalpa Das <kalpadas599@gmail.com>
2025-06-15 17:24:53 +0530

    Update TCP_multi_client.java

--- TCP_multi_client.java
+++ TCP_multi_client.java
@@ -1,35 +1,35 @@
+
 import java.io.*;
 import java.net.*;
-public class MultiClient
-{
-  public static void main(String[] args) throws Exception
-  {
-     Socket sock = new Socket("127.0.0.1", 3000);
-                               // reading from keyboard (keyRead object)
 
-     BufferedReader keyRead = new BufferedReader(new InputStreamReader(System.in));
+public class TCP_multi_client {
 
-                              // sending to server (pwrite object)
-     OutputStream ostream = sock.getOutputStream(); 
-     PrintWriter pwrite = new PrintWriter(ostream, true);
+    public static void main(String[] args) throws Exception {
+        Socket sock = new Socket("127.0.0.1", 3000);
+        // reading from keyboard (keyRead object)
 
-                              // receiving from server ( receiveRead  object)
-     InputStream istream = sock.getInputStream();
-     BufferedReader receiveRead = new BufferedReader(new InputStreamReader(istream));
+        BufferedReader keyRead = new BufferedReader(new InputStreamReader(System.in));
 
-     System.out.println("Start the Chat, type and press Enter key");
+        // sending to server (pwrite object)
+        OutputStream ostream = sock.getOutputStream();
+        PrintWriter pwrite = new PrintWriter(ostream, true);
 
-     String receiveMessage, sendMessage;    
-           
-     while(true)
-     {
-        sendMessage = keyRead.readLine();  // keyboard reading
-        pwrite.println(sendMessage);       // sending to server
-        pwrite.flush();                    // flush the data
-        if((receiveMessage = receiveRead.readLine()) != null) //receive from server
-        {
-            System.out.println(receiveMessage); // displaying at DOS prompt
-        }         
-      }               
-    }                    
-}                        
+        // receiving from server ( receiveRead  object)
+        InputStream istream = sock.getInputStream();
+        BufferedReader receiveRead = new BufferedReader(new InputStreamReader(istream));
+
+        System.out.println("Start the Chat, type and press Enter key");
+
+        String receiveMessage, sendMessage;
+
+        while (true) {
+            sendMessage = keyRead.readLine();  // keyboard reading
+            pwrite.println(sendMessage);       // sending to server
+            pwrite.flush();                    // flush the data
+            if ((receiveMessage = receiveRead.readLine()) != null) //receive from server
+            {
+                System.out.println(receiveMessage); // displaying at DOS prompt
+            }
+        }
+    }
+}
