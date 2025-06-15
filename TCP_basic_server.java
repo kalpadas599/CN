ffffec283fab979315d19afd55c20fcd7cec2dfc
Kalpa Das <kalpadas599@gmail.com>
2025-06-15 17:18:23 +0530

    Update TCP_basic_server.java

--- TCP_basic_server.java
+++ TCP_basic_server.java
@@ -1,16 +1,14 @@
-import java.net.*;
-import java.io.*;
 
-class BasicServer
-{
-public static void main(String arg[])
-{
-try
-{
-System.out.println("Waiting for Client.....");
-ServerSocket serv=new ServerSocket(3000);
-Socket sc=serv.accept();
-}
-catch(Exception e){}
-}
+import java.net.*;
+
+class TCP_basic_server {
+
+    public static void main(String arg[]) {
+        try {
+            System.out.println("Waiting for Client.....");
+            ServerSocket serv = new ServerSocket(3000);
+            Socket sc = serv.accept();
+        } catch (Exception e) {
+        }
+    }
 }
