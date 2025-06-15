8ab98d9fcb8e1d904ab4f56d29e1bb6d75e4e2df
Kalpa Das <kalpadas599@gmail.com>
2025-06-15 17:18:03 +0530

    Update TCP_basic_client.java

--- TCP_basic_client.java
+++ TCP_basic_client.java
@@ -1,16 +1,13 @@
-import java.net.*;
-import java.io.*;
 
-class BasicClient
-{
-public static void main(String arg[])
-{
-try
-{
-Socket clisc=new Socket("localhost",3000);
-System.out.println("*****Find Server******");
-}
-catch(Exception e){
-}
-}
+import java.net.*;
+
+class TCP_basic_client {
+
+    public static void main(String arg[]) {
+        try {
+            Socket clisc = new Socket("localhost", 3000);
+            System.out.println("*****Find Server******");
+        } catch (Exception e) {
+        }
+    }
 }
